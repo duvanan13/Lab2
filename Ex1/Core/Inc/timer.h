10d94775c2ac8b5a30580def44a748409dc7f44e
duvanan13 <duvananproxxx2@gmail.com>
2022-10-18 21:36:18 +0700

    Lab2

---
+++ Ex1/Core/Inc/timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: 5 Oct 2022
+ *      Author: duvan
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+
+#endif /* INC_TIMER_H_ */
